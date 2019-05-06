a3883444cafc528f2db23d1c78fc427a501053c3
Quill McConnell <quill8197@yahoo.com>
2019-05-06 16:13:20 -0400

    Added a talk and sleep method then displayed them in the view

--- pet.php
+++ pet.php
@@ -9,4 +9,14 @@ class Pet
     {
         echo '<p>'.$this->_name.' is eating.</p>';
     }
+
+    function talk()
+    {
+        echo '<p>'.$this->_name.' is talking.</p>';
+    }
+
+    function sleep()
+    {
+        echo '<p>'.$this->_name.' is sleeping.</p>';
+    }
 }
